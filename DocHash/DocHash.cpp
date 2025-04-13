15f3ad87a699f532abed19e89b38dadc01c549d8
Lee Hyung Won <ijhwlee@gmail.com>
2025-04-13 19:57:24 +0900

    generate hash and save into file

--- DocHash/DocHash.cpp
+++ DocHash/DocHash.cpp
@@ -1,8 +1,12 @@
 ﻿// DocHash.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
+#include <iostream>
 #include "framework.h"
+#include "commdlg.h"
 #include "DocHash.h"
+#include <string>
+using namespace std;
 
 #define MAX_LOADSTRING 100
 
@@ -16,43 +20,46 @@ ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void                GetHash(HWND, char *, int);
+void                PrintHash(char*, int, int);
+void                MakeHashString(char*, char*, int);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+  _In_opt_ HINSTANCE hPrevInstance,
+  _In_ LPWSTR    lpCmdLine,
+  _In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: 여기에 코드를 입력합니다.
+  // TODO: 여기에 코드를 입력합니다.
 
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_DOCHASH, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+  // 전역 문자열을 초기화합니다.
+  LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+  LoadStringW(hInstance, IDC_DOCHASH, szWindowClass, MAX_LOADSTRING);
+  MyRegisterClass(hInstance);
 
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
+  // 애플리케이션 초기화를 수행합니다:
+  if (!InitInstance(hInstance, nCmdShow))
+  {
+    return FALSE;
+  }
+
+  HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DOCHASH));
+
+  MSG msg;
+
+  // 기본 메시지 루프입니다:
+  while (GetMessage(&msg, nullptr, 0, 0))
+  {
+    if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
     {
-        return FALSE;
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
     }
+  }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DOCHASH));
-
-    MSG msg;
-
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
+  return (int)msg.wParam;
 }
 
 
@@ -64,23 +71,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+  WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+  wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DOCHASH));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DOCHASH);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DOCHASH));
+  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_DOCHASH);
+  wcex.lpszClassName = szWindowClass;
+  wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+  return RegisterClassExW(&wcex);
 }
 
 //
@@ -95,20 +102,20 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+  hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+  HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, 0, WIN_SIZE_X, WIN_SIZE_Y, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+  if (!hWnd)
+  {
+    return FALSE;
+  }
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
 
-   return TRUE;
+  return TRUE;
 }
 
 //
@@ -123,58 +130,167 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
+  char hash[HASH_SIZE /8] = { 0 };
+  switch (message)
+  {
+  case WM_COMMAND:
+  {
+    int wmId = LOWORD(wParam);
+    // 메뉴 선택을 구문 분석합니다:
+    switch (wmId)
     {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
+    case IDM_OPEN:
+      OutputDebugString(L"============== Open Menu\n");
+      GetHash(hWnd, hash, HASH_SIZE);
+      break;
+    case IDM_ABOUT:
+      DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+      break;
+    case IDM_EXIT:
+      DestroyWindow(hWnd);
+      break;
     default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+      return DefWindowProc(hWnd, message, wParam, lParam);
     }
-    return 0;
+  }
+  break;
+  case WM_PAINT:
+  {
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hWnd, &ps);
+    // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+    EndPaint(hWnd, &ps);
+  }
+  break;
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+  return 0;
 }
 
 // 정보 대화 상자의 메시지 처리기입니다.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+  UNREFERENCED_PARAMETER(lParam);
+  switch (message)
+  {
+  case WM_INITDIALOG:
+    return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
+  case WM_COMMAND:
+    if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+    {
+      EndDialog(hDlg, LOWORD(wParam));
+      return (INT_PTR)TRUE;
     }
-    return (INT_PTR)FALSE;
+    break;
+  }
+  return (INT_PTR)FALSE;
+}
+
+void GetHash(HWND hWnd, char* hash, int size)
+{
+  char buffer[HASH_SIZE /8] = { 0 };
+  memset(hash, 0x00, size / 8); // size is bit size
+  OPENFILENAME OFN;
+  TCHAR filePathName[256] = L"";
+  TCHAR lpstrFile[256] = L"";
+  static TCHAR filter[] = L"모든 파일\0*.*\0PDF 파일\0*.pdf";
+  memset(&OFN, 0x00, sizeof(OPENFILENAME));
+  OFN.lStructSize = sizeof(OPENFILENAME);
+  OFN.hwndOwner = hWnd;
+  OFN.lpstrFilter = filter;
+  OFN.lpstrFile = lpstrFile;
+  OFN.nMaxFile = 256;
+  OFN.lpstrInitialDir = L".";
+  if (GetOpenFileName(&OFN) != 0)
+  {
+    wsprintf(filePathName, L"%s 파일을 열겠습니까?", OFN.lpstrFile);
+    MessageBox(hWnd, filePathName, L"열기 선택", MB_OK);
+    TCHAR msg[256] = L"";
+    wsprintf(msg, L"Files name is %s\n", OFN.lpstrFile);
+    TCHAR msg1[16] = { 0 };
+    OutputDebugString(msg);
+    FILE* pFile = NULL;
+    int count = 0;
+    if (_wfopen_s(&pFile, OFN.lpstrFile, L"rb") == 0 && pFile != NULL)
+    {
+      while (!feof(pFile))
+      {
+        memset(buffer, 0x00, HASH_SIZE / 8);
+        fread_s(buffer, HASH_SIZE / 8, HASH_SIZE / 8, 1, pFile);
+        for (int i = 0; i < size / 8; i++)
+        {
+          hash[i] = hash[i] ^ buffer[i];
+        }
+        //wsprintf(msg1, L"Hash Resuls %d\n", count++);
+        //OutputDebugString(msg1);
+        //PrintHash(hash, HASH_SIZE, 32);
+        //PrintHash(buffer, HASH_SIZE, 32);
+      }
+      fclose(pFile);
+      OutputDebugString(L"============= HASH RESULT ========================\n");
+      PrintHash(hash, HASH_SIZE, 32);
+      FILE* hashFile = NULL;
+      fopen_s(&hashFile, "hash.dat", "wb");
+      if (hashFile != NULL)
+      {
+        fwrite(hash, sizeof(char), HASH_SIZE / 8, hashFile);
+        fclose(hashFile);
+      }
+      char hashStr[HASH_SIZE / 8 * 2+1] = { 0 }; // include the final null character
+      MakeHashString(hashStr, hash, HASH_SIZE);
+      char msg2[512] = { 0 };
+      for (int i = 0; i < HASH_SIZE / 8 * 2; i++)
+      {
+        msg2[i] = hashStr[i];
+      }
+      msg2[HASH_SIZE / 8 * 2] = '\n';
+      OutputDebugStringA(msg2);
+      FILE* hashStrFile = NULL;
+      fopen_s(&hashStrFile, "hashStr.dat", "wb");
+      if (hashStrFile != NULL)
+      {
+        fwrite(hashStr, sizeof(char), HASH_SIZE / 8 * 2 + 1, hashStrFile);
+        fclose(hashStrFile);
+      }
+    }
+    else
+    {
+      wsprintf(msg, L"파일 %s을 열지 못했습니다.\n", OFN.lpstrFile);
+      MessageBox(hWnd, msg, L"열기 실패", MB_OK);
+    }
+  }
+}
+
+void PrintHash(char* hash, int size, int line_width)
+{
+  TCHAR msg1[16] = { 0 };
+  for (int i = 0; i < size / 8; i++)
+  {
+    if ((i + 1) % line_width == 0)
+    {
+      wsprintf(msg1, L"%02X\n", hash[i] & 0xFF);
+    }
+    else
+    {
+      wsprintf(msg1, L"%02X ", hash[i] & 0xFF);
+    }
+    OutputDebugString(msg1);
+  }
+}
+
+void MakeHashString(char* hashStr, char*hash, int size)
+{
+  char line[8] = { 0 };
+  for (int i = 0; i < size / 8; i++)
+  {
+    memset(line, 0x00, 8);
+    sprintf_s(line, 8, "%02X", hash[i] & 0xFF);
+    hashStr[2 * i] = line[0];
+    hashStr[2 * i+1] = line[1];
+  }
 }
--- DocHash/DocHash.h
+++ DocHash/DocHash.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #include "resource.h"
+
+#define WIN_SIZE_X 400
+#define WIN_SIZE_Y 200
+#define HASH_SIZE 1024
